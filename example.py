948065886dd892c68df7bb4317919dcc10f717dd
Antony Harfield <antonyharfield@gmail.com>
2019-07-17 10:30:43 +0700

    CA-1193 Praat files can be overlapping

--- example.py
+++ example.py
@@ -1,4 +1,13 @@
 import rfcx
 
-client = rfcx.Client()
-client.authenticate()
+# client = rfcx.Client()
+# client.authenticate()
+
+tg = rfcx.TextGrid.fromFile('eb17adb8-3c12-40ea-9e39-73083889a4d1.textgrid', strict=False)
+
+intervals = tg[0]
+for interval in intervals:
+    x1 = interval.minTime
+    x2 = interval.maxTime
+    label = interval.mark
+    print(x1, x2, label)
--- rfcx/_textgrid.py
+++ rfcx/_textgrid.py
@@ -211,10 +211,6 @@ class Interval(object):
         if hasattr(other, 'minTime'):
             if self.strict and self.overlaps(other):
                 raise (ValueError(self, other))
-            elif self.overlaps(other):
-                logging.warning("Overlap for interval %s: (%f, %f)",
-                    self.mark, self.minTime, self.maxTime)
-                return self.minTime < other.minTime
             return self.minTime < other.minTime
         elif hasattr(other, 'time'):
             return self.maxTime < other.time
@@ -225,10 +221,6 @@ class Interval(object):
         if hasattr(other, 'maxTime'):
             if self.strict and self.overlaps(other):
                 raise (ValueError(self, other))
-            elif self.overlaps(other):
-                logging.warning("Overlap for interval %s: (%f, %f)",
-                    self.mark, self.minTime, self.maxTime)
-                return self.minTime < other.minTime
             return self.maxTime > other.maxTime
         elif hasattr(other, 'time'):
             return self.minTime > other.time
@@ -245,12 +237,6 @@ class Interval(object):
         if hasattr(other, 'minTime') and hasattr(other, 'maxTime'):
             if self.strict and self.overlaps(other):
                 raise ValueError(self, other)
-                # this returns the two intervals, so user can patch things
-                # up if s/he so chooses
-            elif self.overlaps(other):
-                logging.warning("Overlap for interval %s: (%f, %f)",
-                    self.mark, self.minTime, self.maxTime)
-                return cmp(self.minTime, other.minTime)
             return cmp(self.minTime, other.minTime)
         elif hasattr(other, 'time'):  # comparing Intervals and Points
             return cmp(self.minTime, other.time) + \
@@ -806,8 +792,8 @@ class TextGrid(object):
     # alternative constructor
 
     @classmethod
-    def fromFile(cls, f, name=None):
-        tg = cls(name=name)
+    def fromFile(cls, f, name=None, strict=True):
+        tg = cls(name=name, strict=strict)
         tg.read(f)
         return tg
 
