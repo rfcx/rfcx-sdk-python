2ad6df712aea85513943f9664aeb835889fd5f05
Nutto55 <44169425+Nutto55@users.noreply.github.com>
2022-10-18 23:52:00 +0700

    - Update sdk latest version

--- package-rfcx/setup.py
+++ package-rfcx/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 REQUIRED_PACKAGES = ['httplib2', 'six']
 
 setup(name='rfcx',
-      version='0.2.2',
+      version='0.2.3',
       url='https://github.com/rfcx/rfcx-sdk-python',
       license='None',
       author='Rainforest Connection',
