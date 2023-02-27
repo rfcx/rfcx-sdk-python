e7d9376f73c483fead8530e52f5063a3092edd4b
Antony Harfield <antonyharfield@hotmail.com>
2023-02-27 13:02:14 +0000

    Bump version

--- package-rfcx/setup.py
+++ package-rfcx/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 REQUIRED_PACKAGES = ['httplib2', 'six', 'requests']
 
 setup(name='rfcx',
-      version='0.2.6',
+      version='0.2.7',
       url='https://github.com/rfcx/rfcx-sdk-python',
       license='None',
       author='Rainforest Connection',
