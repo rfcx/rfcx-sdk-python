17f18477ddcfba6160571a54d4c068951c989d5c
TooSeriuz <frongs@rfcx.org>
2024-01-16 15:28:41 +0700

    Add docs, and change example

--- package-rfcx/example-classifier.py
+++ package-rfcx/example-classifier.py
@@ -17,11 +17,11 @@ for i, classification in enumerate(possible_classifications):
 # classification_values = [c['value'] for c in classifications]
 # print(f'Selected classification values: {",".join(classification_values)}')
 classification_values = [
-    # model_output_class:rfcx_classification_value
-    'Sciurus_carolinensis_simple_call:sciurus_carolinensis_simple_call_1',
-    'Sciurus_carolinensis_simple_call_2:sciurus_carolinensis_simple_call_2', 
-    'Sciurus_vulgaris_simple_call:sciurus_vulgaris_simple_call_1', 
-    'Sciurus_vulgaris_simple_call_2:sciurus_vulgaris_simple_call_2']
+    # model_output_class[:rfcx_classification_value][:ignore_threshold]
+    'Sciurus_carolinensis_simple_call:sciurus_carolinensis_simple_call_1:0.4',
+    'Sciurus_carolinensis_simple_call_2:sciurus_carolinensis_simple_call_2:0.2', 
+    'Sciurus_vulgaris_simple_call:sciurus_vulgaris_simple_call_1:1.0', 
+    'Sciurus_vulgaris_simple_call_2:sciurus_vulgaris_simple_call_2:0.1']
 
 # Upload the model
 filepath = 'squirrel.tar.gz'
